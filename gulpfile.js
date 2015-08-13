e83f66b171e36010ca2ab9f96063fa570a2d7561
Ian Seabrook <ian@ianseabrook.com>
2015-08-13 19:08:15 +0100

    Added eslint
    with support for babel and airbnb js style guide

--- gulpfile.js
+++ gulpfile.js
@@ -23,9 +23,16 @@ var AUTOPREFIXER_BROWSERS = [
 gulp.task('eslint', function () {
   return gulp.src('app/scripts/**/*.js')
     .pipe(reload({stream: true, once: true}))
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
+    // eslint() attaches the lint output to the eslint property
+    // of the file object so it can be used by other modules.
+    .pipe($.eslint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe($.eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failOnError last.
+    .pipe($.eslint.failOnError());
+    // .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
 });
 
 // Compile Babel and Concatenate JavaScript
